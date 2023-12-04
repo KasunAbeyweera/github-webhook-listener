43300adb6c3e42f8ad05ae1534be4f81a4c895c9
Kasun Abeyweera <64094014+KasunAbeyweera@users.noreply.github.com>
2023-12-04 15:01:11 +0530

    Initial commit

--- main.py
+++ main.py
@@ -4,28 +4,22 @@ import hashlib
 from dotenv import load_dotenv
 import os
 
-# Load environment variables from .env file
 load_dotenv()
 
 app = Flask(__name__)
 
-# Get the GitHub webhook secret from the environment variable
 webhook_secret = os.getenv('GITHUB_WEBHOOK_SECRET')
 
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    # Validate GitHub signature
     signature = request.headers.get('X-Hub-Signature-256')
     if not is_valid_signature(request.data, signature):
         abort(403, 'Invalid signature')
 
-    # Parse the JSON payload
     payload = request.get_json()
 
-    # Process the payload and handle changes
     print('Received GitHub webhook:', payload)
-    # Add your logic to handle changes here
 
     return 'Webhook received successfully', 200
 
