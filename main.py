662be468c3c9060f37ee5ed83ce443a538098059
Kasun Abeyweera <64094014+KasunAbeyweera@users.noreply.github.com>
2023-12-19 09:39:33 +0530

    Change the port

--- main.py
+++ main.py
@@ -1,33 +1,17 @@
-from flask import Flask, request, abort
-import hmac
-import hashlib
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-webhook_secret = os.getenv('GITHUB_WEBHOOK_SECRET')
-
-
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    signature = request.headers.get('X-Hub-Signature-256')
-    if not is_valid_signature(request.data, signature):
-        abort(403, 'Invalid signature')
+    data = request.json  # Assuming the incoming data is in JSON format
 
-    payload = request.get_json()
-
-    print('Received GitHub webhook:', payload)
-
-    return 'Webhook received successfully', 200
-
-
-def is_valid_signature(data, signature):
-    expected_signature = 'sha256=' + hmac.new(webhook_secret.encode(), data, hashlib.sha256).hexdigest()
-    return hmac.compare_digest(signature, expected_signature)
-
+    # Extract information from the GitHub payload
+    repo_name = data.get('repository', {}).get('name')
+    commit_message = data.get('head_commit', {}).get('message')
+    print(f"New commit in repository '{repo_name}': {commit_message}")
+    return jsonify({'status': 'success'})
 
 if __name__ == '__main__':
-    app.run(port=3000)
+    port = 9090
+    app.run(debug=True, port=port)
