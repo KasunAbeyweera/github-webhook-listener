812e259fec2d5c52dc69001c8b2f9f222d726f6c
Kasun Abeyweera <64094014+KasunAbeyweera@users.noreply.github.com>
2023-12-19 11:14:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
 from flask import Flask, request, jsonify
+from flasgger import Swagger
 
 app = Flask(__name__)
+swagger = Swagger(app)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
+    """
+    Endpoint that receives GitHub webhooks.
+
+    ---
+    parameters:
+      - name: body
+        in: body
+        required: true
+        schema:
+          type: object
+          properties:
+            repository:
+              type: object
+              properties:
+                name:
+                  type: string
+                # Add more properties as needed
+            head_commit:
+              type: object
+              properties:
+                message:
+                  type: string
+                # Add more properties as needed
+    responses:
+      200:
+        description: Success
+        schema:
+          type: object
+          properties:
+            status:
+              type: string
+    """
     data = request.json  # Assuming the incoming data is in JSON format
 
     # Extract information from the GitHub payload
